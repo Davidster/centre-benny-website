f69b669e3636a133cf9c38f560af2b7b85e8daeb
David Huculak <davidhuculak5@gmail.com>
2019-12-22 17:12:10 -0500

    fix variable usages

--- restore.sh
+++ restore.sh
@@ -5,19 +5,16 @@
 set -e
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
 if [ $# -eq 0 ]; then
-    echo "No arguments provided"
     echo "Usage: $0 BACKUP_S3_OBJECT_NAME"
     exit 1
 fi
 
 BACKUP_FILE_NAME=$1
-
-cd /root
 
 # download backup from S3
 aws s3 cp s3://$BACKUP_S3_BUCKET/$BACKUP_FILE_NAME .
@@ -25,9 +22,9 @@ aws s3 cp s3://$BACKUP_S3_BUCKET/$BACKUP_FILE_NAME .
 # perform restore
 $BITNAMI_DIR/ctlscript.sh stop
 rm -rf /tmp/bitnami-backup
-mv /opt/bitnami /tmp/bitnami-backup
-mkdir /opt/bitnami
-tar -pxzvf $BACKUP_FILE_NAME -C /opt/bitnami
+mv $BITNAMI_DIR /tmp/bitnami-backup
+mkdir $BITNAMI_DIR
+tar -pxzvf $BACKUP_FILE_NAME -C $BITNAMI_DIR
 
 # clean up
 rm $BACKUP_FILE_NAME
